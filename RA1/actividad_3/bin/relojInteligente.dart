ff1d79af374d6fa0b1068bf9e5c23fe66c37497c
Abril Palau Pardillos <abrilpalau14@gmail.com>
2025-10-27 17:49:56 +0100

    metodo de mostrarNotificaciones() del reloj inteligente hehco

--- RA1/actividad_3/bin/relojInteligente.dart
+++ RA1/actividad_3/bin/relojInteligente.dart
@@ -25,4 +25,24 @@ class RelojInteligente extends Dispositivo {
     final gpsStr = GPS ? 'Sí' : 'No';
     return '${super.toString()}, GPS: $gpsStr, Tamaño Pantalla: ${tamanioPantalla} pulgadas, Tipo Correa: $tipoCorrea';
   }
+
+  void mostrarNotificaciones() {
+    print(
+      'Mostrando notificaciones en pantalla de $tamanioPantalla pulgadas...',
+    );
+
+    //SIMULACION DE NOTIFICACIONES
+    List<String> notificaciones = [
+      'Nuevo correo de soporte@empresa.com',
+      'Recordatorio: Reunión a las 18:00',
+      'Mensaje de Ana: "¿Vamos al cine?"',
+      'Actualización disponible para el sistema',
+    ];
+
+    for (var nota in notificaciones) {
+      print(nota);
+    }
+
+    print('Fin de las notificaciones.');
+  }
 }
